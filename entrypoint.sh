a5544355fcf332afd7bcd4e4bb5611c368126608
Leandro Voltolino <leandro@voltolino.com>
2023-01-31 22:37:25 -0300

    updated django, docker and front modules versions

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,14 @@
 python manage.py migrate
 yarn install --frozen-lockfile --non-interactive --no-progress --ignore-optional
 
+if [ ! -d "_logs" ]
+then
+    mkdir _logs
+fi
+
+if [ ! -d "_media" ]
+then
+    mkdir _media
+fi
+
 exec "$@"
